07a0eea99a4320abccb7a6e9d0909c4eae05f8b2
Guilherme Bonilha <bonilha.rogante@gmail.com>
2022-11-16 16:08:32 -0300

    aplicação angular dio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoursesComponent } from './courses/courses.component';
+import { StarComponent } from './star/star.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoursesComponent,
+    StarComponent  
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
